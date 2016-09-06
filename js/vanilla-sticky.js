2d45ed6fad3e36ce24865e3f83e9e337f2abec1c
Kevin Rocher <kevin@rocher-dev.com>
2016-09-06 23:45:38 +0200

    v 0.2
    * Performance checks.
    * Basic options.

--- js/vanilla-sticky.js
+++ js/vanilla-sticky.js
@@ -1,6 +1,6 @@
 /*
  * Plugin Name: Vanilla-JS Sticky
- * Version: 0.1.1
+ * Version: 0.2
  * Plugin URL: https://github.com/Darklg/JavaScriptUtilities
  * JavaScriptUtilities Vanilla-JS may be freely distributed under the MIT license.
  */
@@ -15,33 +15,45 @@
 
 function vanilla_sticky_launch(elements) {
     'use strict';
-    /* Inject styles */
-    var cssRules = '[data-sticky-top="1"] {position: fixed!important;top: 0!important;bottom: auto!important;}' + '[data-sticky-bottom="1"] {position: absolute!important;top: auto!important;bottom: 0!important;}',
-        nodeCSS = document.createElement('style');
-    nodeCSS.innerHTML = cssRules;
-    document.body.appendChild(nodeCSS);
+
+    vanilla_sticky_cssrules();
+
     /* Call sticky for each element */
     for (var i = 0, len = elements.length; i < len; i++) {
         new vanilla_sticky(elements[i]);
     }
 }
 
-function vanilla_sticky(el) {
+function vanilla_sticky_cssrules() {
     'use strict';
 
-    var elPosition,
+    /* Inject styles */
+    var cssRules = '[data-sticky-top="1"] {position: fixed!important;top: 0!important;bottom: auto!important;}' + '[data-sticky-bottom="1"] {position: absolute!important;top: auto!important;bottom: 0!important;}',
+        nodeCSS = document.createElement('style');
+    nodeCSS.innerHTML = cssRules;
+    document.body.appendChild(nodeCSS);
+}
+
+function vanilla_sticky(el, opts) {
+    'use strict';
+
+    opts = opts || {};
+
+    var elParent = opts.elParent || el.parentNode,
+        elPosition,
         elParentPosition,
         elStatus = -1;
 
     function init() {
         /* Prevent double launch */
         if (el.getAttribute('vanilla-sticky') == '1') {
-            update_positions(el);
-            set_sticky_element(el);
+            update_positions();
+            set_sticky_element();
             return;
         }
         el.setAttribute('vanilla-sticky', '1');
 
+        /* Setup */
         set_elements();
         set_events();
     }
@@ -49,48 +61,50 @@ function vanilla_sticky(el) {
     function set_elements() {
         el.style.position = 'absolute';
         el.style.top = 0;
-        el.parentNode.style.position = 'relative';
+        elParent.style.position = 'relative';
     }
 
     function set_events() {
         /* Initial check */
-        update_positions(el);
-        set_sticky_element(el);
+        update_positions();
+        set_sticky_element();
+
         /* When scrolling, set sticky status */
-        window.addEventListener('scroll', function() {
-            set_sticky_element(el);
-        });
+        window.addEventListener('scroll', set_sticky_element);
+
         /* When page is loaded, update positions */
-        window.addEventListener('load', function() {
-            update_positions(el);
-            set_sticky_element(el);
-        });
+        window.addEventListener('load', update_and_sticky);
+
         /* When resizing, update positions */
-        window.addEventListener('resize', function() {
-            update_positions(el);
-            set_sticky_element(el);
-        });
+        window.addEventListener('resize', update_and_sticky);
+    }
+
+    function update_and_sticky() {
+        update_positions();
+        set_sticky_element();
     }
 
     /* Update element positions */
-    function update_positions(el) {
+    function update_positions() {
         elPosition = getElementOffset(el);
-        elParentPosition = getElementOffset(el.parentNode);
+        elParentPosition = getElementOffset(elParent);
     }
 
     /* Set sticky status on element */
-    function set_sticky_element(el) {
+    function set_sticky_element() {
+        var offsetTop = getBodyScrollTop();
+
         /* Scroll level */
         var startScroll = elParentPosition.top,
             endScroll = elParentPosition.bottom - elPosition.height;
 
         /* Scroll status */
-        var scrollBeforeElement = (window.pageYOffset < startScroll),
-            scrollOverElement = (window.pageYOffset >= startScroll && window.pageYOffset < endScroll),
-            scrollAfterElement = (window.pageYOffset >= endScroll);
+        var scrollBeforeElement = (offsetTop < startScroll),
+            scrollOverElement = (offsetTop >= startScroll && offsetTop < endScroll),
+            scrollAfterElement = (offsetTop >= endScroll);
 
-        /* scroll before top of the element : no sticky */
-        if (scrollBeforeElement && elStatus !== 0) {
+        /* scroll before top of the element or parent smaller than container : no sticky */
+        if ((scrollBeforeElement && elStatus !== 0) || (elParentPosition.height < elPosition.height)) {
             el.setAttribute('data-sticky-top', 0);
             el.setAttribute('data-sticky-bottom', 0);
             elStatus = 0;
@@ -115,17 +129,18 @@ function vanilla_sticky(el) {
 
     }
 
+    function getBodyScrollTop() {
+        return window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    }
+
+    function getBodyScrollLeft() {
+        return window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft || 0;
+    }
+
     /* Element Offset */
-    function getElementOffset(el) {
-        var getBodyScrollTop = function() {
-            return window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-        };
+    function getElementOffset(item) {
 
-        var getBodyScrollLeft = function() {
-            return window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft || 0;
-        };
-
-        var clientRect = el.getBoundingClientRect(),
+        var clientRect = item.getBoundingClientRect(),
             top = clientRect.top + getBodyScrollTop(),
             left = clientRect.left + getBodyScrollLeft(),
             right = clientRect.width + left,
