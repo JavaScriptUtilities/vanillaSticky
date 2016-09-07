b42c0fdf9ea688128bb9b02cd954e567ff17bafb
Kevin Rocher <kevin@rocher-dev.com>
2016-09-07 22:57:57 +0200

    v 0.3.0
    * Add options : reference element, z-index for parent, use the parent top as a higher limit.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # vanillaSticky
+
 Make an element sticky
+
+## How to use
+
+Include the JS file in your HEAD.
+
+```html
+<script src="js/vanilla-sticky.min.js"></script>
+```
+
+```javascript
+new vanilla_sticky_launch(document.querySelectorAll('.sticky-element'));
+```
--- js/vanilla-sticky.js
+++ js/vanilla-sticky.js
@@ -1,6 +1,6 @@
 /*
  * Plugin Name: Vanilla-JS Sticky
- * Version: 0.2.1
+ * Version: 0.3.0
  * Plugin URL: https://github.com/Darklg/JavaScriptUtilities
  * JavaScriptUtilities Vanilla-JS may be freely distributed under the MIT license.
  */
@@ -13,23 +13,31 @@
   Launch multiple elements
 ---------------------------------------------------------- */
 
-function vanilla_sticky_launch(elements) {
+function vanilla_sticky_launch(elements, opts) {
     'use strict';
 
-    vanilla_sticky_cssrules();
+    opts = opts || {};
+
+    vanilla_sticky_cssrules(opts);
 
     /* Call sticky for each element */
     for (var i = 0, len = elements.length; i < len; i++) {
-        new vanilla_sticky(elements[i]);
+        new vanilla_sticky(elements[i], opts);
     }
 }
 
-function vanilla_sticky_cssrules() {
+function vanilla_sticky_cssrules(opts) {
     'use strict';
 
+    opts = opts || {};
+
     /* Inject styles */
-    var cssRules = '[data-sticky-top="1"] {position: fixed!important;top: 0!important;bottom: auto!important;}' + '[data-sticky-bottom="1"] {position: absolute!important;top: auto!important;bottom: 0!important;}',
+    var cssRules,
         nodeCSS = document.createElement('style');
+
+    cssRules = '[data-sticky-top="1"] {position: fixed!important;top: 0!important;bottom: auto!important;}';
+    cssRules += '[data-sticky-bottom="1"] {position: absolute!important;top: auto!important;bottom: 0!important;}';
+
     nodeCSS.innerHTML = cssRules;
     document.body.appendChild(nodeCSS);
 }
@@ -39,9 +47,13 @@ function vanilla_sticky(el, opts) {
 
     opts = opts || {};
 
-    var elParent = opts.elParent || el.parentNode,
+    var elReference = opts.elReference || el.parentNode,
+        useParentTop = opts.useParentTop || false,
+        zIndexParent = opts.zIndexParent || 2,
+        elParentTop,
         elPosition,
         elParentPosition,
+        elReferencePosition,
         elStatus = -1;
 
     function init() {
@@ -61,7 +73,12 @@ function vanilla_sticky(el, opts) {
     function set_elements() {
         el.style.position = 'absolute';
         el.style.top = 0;
-        elParent.style.position = 'relative';
+        elReference.style.position = 'relative';
+        elReference.style.zIndex = zIndexParent;
+        if (useParentTop && el.parentNode != elReference) {
+            el.parentNode.style.position = 'relative';
+            el.parentNode.style.zIndex = zIndexParent;
+        }
     }
 
     function set_events() {
@@ -87,7 +104,12 @@ function vanilla_sticky(el, opts) {
     /* Update element positions */
     function update_positions() {
         elPosition = getElementOffset(el);
-        elParentPosition = getElementOffset(elParent);
+        elReferencePosition = getElementOffset(elReference);
+        elParentTop = elReferencePosition.top;
+        if (useParentTop && el.parentNode != elReference) {
+            elParentPosition = getElementOffset(el.parentNode);
+            elParentTop = elParentPosition.top;
+        }
     }
 
     /* Set sticky status on element */
@@ -95,8 +117,8 @@ function vanilla_sticky(el, opts) {
         var offsetTop = getBodyScrollTop();
 
         /* Scroll level */
-        var startScroll = elParentPosition.top,
-            endScroll = elParentPosition.bottom - elPosition.height;
+        var startScroll = elParentTop,
+            endScroll = elReferencePosition.bottom - elPosition.height;
 
         /* Scroll status */
         var scrollBeforeElement = (offsetTop < startScroll),
@@ -104,7 +126,7 @@ function vanilla_sticky(el, opts) {
             scrollAfterElement = (offsetTop >= endScroll);
 
         /* scroll before top of the element or parent smaller than container : no sticky */
-        if ((scrollBeforeElement && elStatus !== 0) || (elParentPosition.height < elPosition.height)) {
+        if ((scrollBeforeElement && elStatus !== 0) || (elReferencePosition.height < elPosition.height)) {
             el.setAttribute('data-sticky-top', 0);
             el.setAttribute('data-sticky-bottom', 0);
             elStatus = 0;
